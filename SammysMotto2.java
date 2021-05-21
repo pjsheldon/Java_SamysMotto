cb09f56592efe7f261c9705e12c07aa69f02423e
pjsheldon <73202918+pjsheldon@users.noreply.github.com>
2021-05-20 21:51:49 -0700

    Add files via upload

---
+++ SammysMotto2.java
@@ -0,0 +1,10 @@
+public class SammysMotto2
+
+{
+	public static void main(String[] args)
+	{
+		System.out.println("SsSsSsSsSsSsSsSsSsSsSsSsSsSsSsSsSsSsSs");
+		System.out.println("Ss Sammy’s makes it fun in the sun. Ss");
+		System.out.println("SsSsSsSsSsSsSsSsSsSsSsSsSsSsSsSsSsSsSs");
+	}
+}
